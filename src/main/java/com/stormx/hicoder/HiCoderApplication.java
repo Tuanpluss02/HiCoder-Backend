b38bb9c374e34b0e0a3b7907ddda6ed49a597923
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2024-01-20 21:51:00 +0700

    first commit

---
+++ src/main/java/com/stormx/hicoder/HiCoderApplication.java
@@ -0,0 +1,13 @@
+package com.stormx.hicoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiCoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiCoderApplication.class, args);
+    }
+
+}
