3e3a38b4842968ff0c8d40df66f8cc0be62e6f6c
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2024-01-22 00:31:32 +0700

    (feat) add config for OpenAPI

---
+++ src/main/java/com/stormx/hicoder/configuration/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package com.stormx.hicoder.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Tuan Do",
+                        email = "tuando@stormx.works",
+                        url = "https://stormx.works/"
+                ),
+                description = "OpenApi documentation for HiCoder Backend",
+                title = "HiCoder Backend",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
