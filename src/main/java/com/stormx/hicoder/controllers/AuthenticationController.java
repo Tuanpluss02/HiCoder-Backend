498cd8722b0a5d7d921555f98de23cdd8989b3e0
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2024-01-22 17:39:56 +0700

    (feat) add userPosts to userDTO

--- src/main/java/com/stormx/hicoder/controllers/AuthenticationController.java
+++ src/main/java/com/stormx/hicoder/controllers/AuthenticationController.java
@@ -17,10 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -50,11 +47,12 @@ public class AuthenticationController {
     @PostMapping("/reset-password")
     public ResponseEntity<?> sendEmailResetPassword(@Valid ResetPasswordDTO resetPasswordDTO, BindingResult bindingResult) throws ValidationException {
         checkValidRequest(bindingResult);
+        authenticationService.sendEmailResetPassword(resetPasswordDTO);
         return ResponseEntity.accepted().build();
     }
 
 
-    @PostMapping("/refresh-token")
+    @GetMapping("/refresh-token")
     public AuthenticationResponse refreshToken(
             HttpServletRequest request,
             HttpServletResponse response
