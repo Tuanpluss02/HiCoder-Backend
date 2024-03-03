f9a48c2d1638d6062ccdeef5a6997746b51f7612
Long <ngxuanlong2k2@gmail.com>
2024-03-03 23:20:47 +0700

    test

--- src/main/java/com/stormx/hicoder/HiCoderBackendApplication.java
+++ src/main/java/com/stormx/hicoder/HiCoderBackendApplication.java
@@ -2,12 +2,19 @@ package com.stormx.hicoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HiCoderBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HiCoderBackendApplication.class, args);
     }
 
+    @GetMapping("")
+    String test() {
+        return "Hello, World!";
+    }
 }
