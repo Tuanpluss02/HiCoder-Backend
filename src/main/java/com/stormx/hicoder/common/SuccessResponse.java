bdc12adc267210a084d3eeb5040c396ac8f4af75
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2024-03-08 20:15:27 +0700

    (chore) update SuccessResponse

--- src/main/java/com/stormx/hicoder/common/SuccessResponse.java
+++ src/main/java/com/stormx/hicoder/common/SuccessResponse.java
@@ -1,24 +1,13 @@
 package com.stormx.hicoder.common;
 
 
-import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
-@Data
-public class SuccessResponse {
-    private String timestamp;
-    private String path;
-    private HttpStatus status;
-    private String message;
-    private Object body;
 
+public class SuccessResponse extends ResponseGeneral {
     public SuccessResponse(HttpStatus status, String message, String path, Object body) {
-        this.timestamp = LocalDateTime.now().toString();
-        this.path = path;
-        this.status = status;
-        this.message = message;
-        this.body = body;
+        super(LocalDateTime.now().toString(), path, status.value(), message, body);
     }
 }
--- src/main/java/com/stormx/hicoder/controllers/UserController.java
+++ src/main/java/com/stormx/hicoder/controllers/UserController.java
@@ -1,7 +1,15 @@
 package com.stormx.hicoder.controllers;
 
+import com.stormx.hicoder.common.ResponseGeneral;
+import com.stormx.hicoder.common.SuccessResponse;
+import com.stormx.hicoder.dto.UserDTO;
+import com.stormx.hicoder.entities.User;
 import com.stormx.hicoder.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +20,11 @@ public class UserController {
 
     private final UserService userService;
 
-//    @GetMapping("/me")
-//    ResponseEntity<ResponseGeneral> getCurrentUserDetail(HttpServletRequest request) {
-//        User currentUser = userService.getCurrentUser();
-//        return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "Get user detail successfully", request.getRequestURI(), currentUser));
-//    }
-
+    @GetMapping("/me")
+    ResponseEntity<ResponseGeneral> getCurrentUserDetail(HttpServletRequest request) {
+        User currentUser = userService.getCurrentUser();
+        UserDTO response = new UserDTO(currentUser);
+        return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "Get user detail successfully", request.getRequestURI(), response));
+    }
 
 }
